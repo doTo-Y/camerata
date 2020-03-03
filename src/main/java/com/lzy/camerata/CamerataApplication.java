4ac6b12f6bb3f44d6453fede986dcc2cfdf84d35
doTo-Y <2414811094@qq.com>
2020-03-03 10:19:14 +0800

    尝试

---
+++ src/main/java/com/lzy/camerata/CamerataApplication.java
@@ -0,0 +1,13 @@
+package com.lzy.camerata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamerataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamerataApplication.class, args);
+    }
+
+}
